3ca0eb9cf02f67909d74e097796c0d7188b53f6b
A Umesh <aumesh685@gmail.com>
2023-07-18 12:54:35 +0530

    reactapptodo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import Todo from './components/todoreact/todo'
-import Temp from './components/weatherapp/temp'
+import Todo from './components/todoreact/todo'
+// import Temp from './components/weatherapp/temp'
 const App = () => {
   return (
     <>
-    {/* <Todo/> */}
-    <Temp/>
+    <Todo/>
+    {/* <Temp/> */}
     </>
   )
 }
